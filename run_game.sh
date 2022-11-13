6143976de5c4cb0dd4059241032b7e9017baafef
danjoh2019 <danjoh2019@skola.goteborg.se>
2022-11-13 15:27:00 +0100

    adding script

---
+++ run_game.sh
@@ -0,0 +1,14 @@
+echo "Daniel Johanssons program"
+mkdir Daniel_Johansson_labb
+cp *.java Daniel_Johansson_labb
+cd Daniel_Johansson_labb
+echo -n "Running game from "
+pwd
+echo "compiling..."
+javac GuessingGame.java
+echo "running..."
+java GuessingGame
+
+echo "Done!"
+rm *.class
+ls
